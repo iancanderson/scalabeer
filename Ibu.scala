c10b48e53e00e2415a8caf4e619911dfaa9fae0d
Ian C. Anderson <ian@iancanderson.com>
2017-07-14 10:38:03 -0400

    Stuff

--- Ibu.scala
+++ Ibu.scala
@@ -1,40 +1,3 @@
-// hopAdditionUtilization : BoilGravity -> HopAddition -> Float
-// hopAdditionUtilization boilGravity hopAddition =
-//     let
-//         gravityFactor =
-//             1.65 * 0.000125 ^ (boilGravity - 1)
-//
-//         timeFactor =
-//             (1 - e ^ (-0.04 * boilMinutes)) / 4.15
-//
-//         boilMinutes =
-//             case hopAddition.boilTime.timeUnit of
-//                 Minute ->
-//                     hopAddition.boilTime.value
-//     in
-//         gravityFactor * timeFactor
-//
-//
-// hopAdditionIbus : Model -> ( ID, HopAddition ) -> Float
-// hopAdditionIbus recipe ( _, hopAddition ) =
-//     let
-//         alphaAcidPercentage =
-//             hopVarietyAlphaAcid hopAddition.variety
-//
-//         alphaAcidUnits =
-//             alphaAcidPercentage * weightInOunces
-//
-//         utilization =
-//             hopAdditionUtilization recipe.boilGravity hopAddition
-//
-//         weightInOunces =
-//             case hopAddition.amount.massUnit of
-//                 Ounce ->
-//                     hopAddition.amount.value
-//     in
-//         alphaAcidUnits * utilization * 75 / gallons (recipe.volume)
-
-
 case class HopAddition(alphaAcid: Double, ounces: Double, boilMinutes: Double)
 case class Recipe(hopAdditions: List[HopAddition])
 
@@ -67,7 +30,7 @@ object Main {
     val hopAdditions = List(
       HopAddition(7.0, 1, 60),
       HopAddition(7.0, 1, 20),
-      HopAddition(7.0, 1, 0),
+      HopAddition(7.0, 1, 0)
     )
     val sierraNevadaPaleAle = Recipe(hopAdditions)
     val snIbus = ibus(sierraNevadaPaleAle)
