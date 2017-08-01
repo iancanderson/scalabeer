9466ad746b6b278b3d569ed741df80dd04792766
Ian C. Anderson <ian@iancanderson.com>
2017-08-01 19:45:28 -0400

    Use case objects for exhaustive matches

--- Ibu.scala
+++ Ibu.scala
@@ -1,16 +1,14 @@
 case class HopAddition(alphaAcid: Double, ounces: Double, boilMinutes: Double)
 
-object VolumeUnit extends Enumeration {
-  type VolumeUnit = Value
-  val Gallon, Liter = Value
-}
-
-import VolumeUnit._
+sealed trait VolumeUnit
+case object Gallon extends VolumeUnit
+case object Liter extends VolumeUnit
 
 case class Volume(amount: Double, unit: VolumeUnit) {
   def inGallons(): Double = {
     unit match {
       case Gallon => amount
+      case Liter => amount / 3.78541
     }
   }
 }
